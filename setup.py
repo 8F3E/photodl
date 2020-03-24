c92605049ba3f2c34af1e21c7e5e82ff0330ce72
8F3E <39588555+8F3E@users.noreply.github.com>
2020-03-24 12:29:02 +0000

    Change version to 0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="photodl",
-    version="0.0.1",
+    version="0.1.0",
     author="8F3E",
     description="A simple python package to download, sort and backup photos "
                 "from an SD card.",
