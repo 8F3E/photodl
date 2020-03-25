81656587aaf9581a6be71e9007e0a3df8a6fe83f
8F3E <39588555+8F3E@users.noreply.github.com>
2020-03-25 12:55:20 +0000

    Show help if no args are sent

--- photodl/__main__.py
+++ photodl/__main__.py
@@ -1,3 +1,6 @@
+import sys
+
+
 def main():
     import argparse
     from pathlib import Path
@@ -9,12 +12,17 @@ def main():
                                                  "tos from an SD card.")
 
     parser.add_argument("paths", metavar="path", type=str, nargs="+",
-                        help="Path(s) to download, sort and backup.")
+                        help="Path(s) to download, sort and backup from.")
 
     parser.add_argument("dest", type=str, nargs=1,
                         help="Path to copy sorted files to.")
 
-    parser.add_argument("--backup", type=str, help="Path to backup files to.")
+    parser.add_argument("--backup", type=str, help="Path to backup files to. "
+                                                   "(You really should...)")
+
+    if len(sys.argv) == 1:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
 
     args = parser.parse_args()
     files = []
@@ -25,6 +33,7 @@ def main():
 
     f = filer.Filer(files, args.dest[0], args.backup)
     f.go()
+    sys.exit(0)
 
 
 if __name__ == "__main__":
